03cf564b41f231a1dfec9bbc23839f08b4d599f9
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-04-29 22:25:28 +0700

    Setup redux store and redux-persist

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from "react";
 import styled from "styled-components";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { accessToken, userSession } from "./store/reducers";
+
+const reducers = combineReducers({
+  accessToken,
+  userSession
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["accessToken", "userSession"]
+};
+
+const persistedReducers = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducers);
+const persistor = persistStore(store);
 
 const Container = styled.div``;
 
-const App = () => <Container />;
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <Container />
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
