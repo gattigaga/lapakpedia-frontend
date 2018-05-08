5eb7e97f73858733b6c1b76d5016d69ca7bcf838
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-05-08 23:28:02 +0700

    Make SearchBar component able to take additional class name

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const Input = styled.input`
-  width: 100%;
+  width: 240px;
   border: 0px;
   border-bottom: 1px solid black;
   padding: 4px 0px;
@@ -13,9 +13,10 @@ const Input = styled.input`
   letter-spacing: 0.1em;
 `;
 
-const SearchBar = ({ keyword, onChange, onPressEnter }) => (
+const SearchBar = ({ className, keyword, onChange, onPressEnter }) => (
   <Input
     type="text"
+    className={className}
     value={keyword}
     onChange={onChange}
     onKeyPress={event => event.key === "Enter" && onPressEnter()}
@@ -26,7 +27,8 @@ const SearchBar = ({ keyword, onChange, onPressEnter }) => (
 SearchBar.propTypes = {
   onChange: PropTypes.func,
   onPressEnter: PropTypes.func,
-  keyword: PropTypes.string
+  keyword: PropTypes.string,
+  className: PropTypes.string
 };
 
 SearchBar.defaultProps = {
