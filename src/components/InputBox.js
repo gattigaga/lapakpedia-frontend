2113597cff368cfbd3fe43314b951873cb016827
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-05-18 18:56:12 +0700

    Add prop 'name' in InputBox component

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 const Container = styled.div`
   width: 100%;
+  margin-bottom: 16px;
 `;
 
 export const Input = styled.input`
@@ -11,6 +12,7 @@ export const Input = styled.input`
   border-bottom: 1px solid ${props => (props.isError ? "red" : "#ccc")};
   outline: none;
   font-size: 14px;
+  font-family: Roboto;
   letter-spacing: 0.1em;
   padding: 8px 0px;
   width: 100%;
@@ -27,6 +29,7 @@ export const Helper = styled.p`
 const InputBox = ({
   placeholder,
   type,
+  name,
   value,
   helperText,
   onChange,
@@ -36,6 +39,7 @@ const InputBox = ({
   <Container>
     <Input
       type={type}
+      name={name}
       value={value}
       onChange={onChange}
       placeholder={placeholder}
@@ -49,6 +53,7 @@ const InputBox = ({
 
 InputBox.propTypes = {
   placeholder: PropTypes.string,
+  name: PropTypes.string,
   value: PropTypes.string,
   type: PropTypes.string,
   helperText: PropTypes.string,
